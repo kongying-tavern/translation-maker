a262896166fd16ab73f60c83f37922bc9dde74c6
Alex Fang <deeno@qq.com>
2021-07-17 14:20:38 +0800

    feat: add subgroup generation CSV for Tevat (#5)
    * fix: fix subgroup with mapper config `useKey: true`
    
    Co-authored-by: PeaceShi <peaceshi@outlook.com>

---
+++ proc/save-subgroups.js
@@ -0,0 +1,27 @@
+const _       = require('lodash');
+const Path    = require('path');
+const FsExtra = require('fs-extra');
+
+module.exports = (base = process.cwd(), translationConfig = {}, target = '', conf = {KeyMapper: {}}) => {
+    if(!target)
+        throw new Error('[SAVE-SUBGROUPS] Target is empty');
+
+    let translations = translationConfig.translations || {};
+    let subgroups    = translationConfig.subgroups || {};
+
+    for(let langCode in translations) {
+        let translation          = translations[langCode] || {};
+        let mapperConf           = _.find(conf.KeyMapper, {langCode}) || {};
+        let translationConverted = mapperConf.useKey ? _.mapValues(translation, (v, i) => i) : translation;
+
+        for(let subgroupName in subgroups) {
+            let subgroupKeys              = subgroups[subgroupName] || [];
+            let subgroupTranslation       = _.pick(translationConverted, subgroupKeys);
+            let subgroupTranslationSorted = _.sortBy(subgroupTranslation, v => subgroupKeys.indexOf(v));
+
+            let translationOutput   = subgroupTranslationSorted.join(',');
+            let translationFilePath = Path.resolve(base, target, `./${langCode}.${subgroupName}.txt`);
+            FsExtra.outputFileSync(translationFilePath, translationOutput, 'utf-8');
+        }
+    }
+};
