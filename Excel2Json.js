f3eb85e85baf4573fc46dfbd74e926e72c4ac2c3
peaceshi <peaceshi@outlook.com>
2021-07-17 13:31:48 +0800

    Revert "feat: add subgroup csv generation for "Tevat" (#3)"
    This reverts commit 102571ec60dfd02aa2f10ce05e649229aa475f7e.

--- Excel2Json.js
+++ Excel2Json.js
@@ -4,7 +4,6 @@ const ProcRmdir            = require('./proc/rmdir');
 const ProcDownloadExcel    = require('./proc/download-excel');
 const ProcRenderExcel      = require('./proc/render-excel');
 const ProcSaveTranslations = require('./proc/save-translations');
-const ProcSaveSubgroups    = require('./proc/save-subgroups');
 const ProcSaveManifest     = require('./proc/save-manifest');
 const ProcPackTranslations = require('./proc/pack-translations');
 
@@ -14,15 +13,13 @@ const ProcPackTranslations = require('./proc/pack-translations');
     await ProcDownloadExcel(__dirname, ConfDownload.DocKey, './resource/excel.xlsx');
 
     // 解析Excel
-    let tlConf = ProcRenderExcel(__dirname, './resource/excel.xlsx', 'YSDTTranslate', ConfMapper);
+    let translations = ProcRenderExcel(__dirname, './resource/excel.xlsx', 'YSDTTranslate', ConfMapper);
 
     // 保存语言文件
     ProcRmdir(__dirname, './output');
-    ProcSaveTranslations(__dirname, tlConf.translations, './output/translations', ConfMapper);
-    ProcSaveSubgroups(__dirname, tlConf, './output/subgroups', ConfMapper);
-    ProcSaveManifest(__dirname, tlConf.translations, './output/translations', ConfMapper);
+    ProcSaveTranslations(__dirname, translations, './output/translations', ConfMapper);
+    ProcSaveManifest(__dirname, translations, './output/translations', ConfMapper);
 
     // 打包成压缩文件
     ProcPackTranslations(__dirname, './output/translations', './output/translations.zip');
-    ProcPackTranslations(__dirname, './output/subgroups', './output/subgroups.zip');
 })();
